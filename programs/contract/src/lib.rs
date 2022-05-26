4d47c42d8125ad8077b9baae0945a0f47a65771d
Thanh Vu <thanh@luongthien.com>
2022-05-26 11:28:07 +0700

    Implement create associated token account if needed

--- programs/contract/src/lib.rs
+++ programs/contract/src/lib.rs
@@ -11,18 +11,13 @@ declare_id!("FhWUGomvmZfN2G2tyhEiaYgjCePg5gF6GC6GA3ccrVRc");
 pub mod contract {
     use anchor_lang::solana_program::{
         entrypoint::ProgramResult,
-        program::invoke_signed,
         system_instruction,
     };
     use anchor_lang::solana_program::program::invoke;
-    use anchor_spl::token;
     use spl_token;
 
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
     pub fn reward(ctx: Context<TransferRewardToken>, lamports: u64) -> ProgramResult {
         msg!("Signer info {:#?}", ctx.accounts.signer.to_account_info());
         invoke(&system_instruction::transfer(ctx.accounts.signer.key,
@@ -69,7 +64,9 @@ pub mod contract {
                                     amount)
     }
 
-        return token::transfer(CpiContext::new(ctx.accounts.token_program.to_account_info(), transfer_ctx), amount)
+    pub fn create_associated_token_account(ctx: Context<CreateAssociatedTokenAccount>) -> Result<()> {
+        msg!("Created associated token account: {:#?}", ctx.accounts.token.to_account_info().key);
+        Ok(())
     }
 }
 
@@ -98,4 +95,24 @@ pub struct TransferSPLToken<'info> {
     pub receiver_account: AccountInfo<'info>, // Associated token account
     /// CHECK: This is not dangerous because we don't read or write from this account
     pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct CreateAssociatedTokenAccount<'info> {
+    #[account(
+    init_if_needed,
+    payer = payer,
+    associated_token::mint = mint,
+    associated_token::authority = owner,
+    )]
+    pub token: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    /// CHECK: unsafe
+    pub owner: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
